29089850e765bb4afb92e2d5090b8b512adf14f2
hafit <hafit.abekrori@students.amikom.ac.id>
2024-09-06 20:56:45 +0700

    add repository for database

--- app/src/main/java/com/haf/artha/data/local/LocalRepository.kt
+++ app/src/main/java/com/haf/artha/data/local/LocalRepository.kt
@@ -1,4 +1,15 @@
 package com.haf.artha.data.local
 
-class LocalRepository {
+import com.haf.artha.data.local.db.AppDao
+import com.haf.artha.data.local.entity.AccountEntity
+import com.haf.artha.data.local.entity.CategoryEntity
+import com.haf.artha.data.local.entity.TransactionEntity
+import kotlinx.coroutines.flow.Flow
+
+class LocalRepository(
+    private val appDao: AppDao
+) {
+    fun getAccounts() : Flow<List<AccountEntity>> = appDao.getAccounts()
+    fun getCategories() : Flow<List<CategoryEntity>> = appDao.getCategories()
+    fun getTransactions() : Flow<List<TransactionEntity>> = appDao.getTransactions()
 }
