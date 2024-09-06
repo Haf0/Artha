6cbc8afa9aa599a713093d71fd3c99d256648197
hafit <hafit.abekrori@students.amikom.ac.id>
2024-09-06 20:53:45 +0700

    change coil to compose image

--- app/src/main/java/com/haf/artha/presentation/splash/SplashScreen.kt
+++ app/src/main/java/com/haf/artha/presentation/splash/SplashScreen.kt
@@ -3,6 +3,7 @@ package com.haf.artha.presentation.splash
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
@@ -11,6 +12,7 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import coil.compose.AsyncImage
@@ -18,32 +20,18 @@ import com.haf.artha.R
 import com.haf.artha.ui.theme.ArthaTheme
 
 
-class SplashActivity : ComponentActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-
-        setContent {
-            ArthaTheme {
-                // A surface container using the 'background' color from the theme
-                splashScreen()
-            }
-        }
-    }
-}
 
 @Preview(showBackground = true)
 @Composable
-fun splashScreen() {
+fun SplashScreen() {
     Box(
         modifier = Modifier
             .fillMaxSize()
             .background(MaterialTheme.colorScheme.background),
         contentAlignment = Alignment.Center
     ) {
-        AsyncImage(
-            model = R.drawable.ic_splash,
-            contentDescription = "Splash Screen Image",
-            modifier = Modifier.size(300.dp)
-        )
+
+        Image(painter = painterResource(id = R.drawable.ic_splash), contentDescription = "Splash Screen Image", modifier = Modifier.size(300.dp))
+
     }
 }
