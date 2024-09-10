e3f9c4e1f836500ea480a9cdc99b286c1a6b129c
hafit <hafit.abekrori@students.amikom.ac.id>
2024-09-10 21:57:34 +0700

    add list local ewallet to Account Item

--- app/src/main/java/com/haf/artha/presentation/component/AccountItem.kt
+++ app/src/main/java/com/haf/artha/presentation/component/AccountItem.kt
@@ -1,11 +1,6 @@
 package com.haf.artha.presentation.component
 
-import androidx.compose.material3.FilterChip
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 
 @Composable
@@ -13,11 +8,7 @@ fun AccountItem(
     modifier: Modifier = Modifier,
     onclick : () -> Unit
 ) {
-    var selected by remember { mutableStateOf(false) }
+    //tommorow add something like category but for account and list all local e wallet
 
-    FilterChip(
-        selected = selected,
-        onClick = { onclick() },
-        label = { /*TODO*/ }
-    )
+    val listEwalletIndonesia = listOf("OVO", "DANA", "LinkAja", "GoPay", "ShopeePay", "PayPal", "Sakuku", "Jenius", "BCA", "BNI", "Mandiri", "BRI", "CIMB Niaga", "Permata", "Maybank", "Panin", "Danamon", "BTPN", "Bukalapak", "Tokopedia", "Blibli", "Alfamart", "Indomaret", "Circle K", "7 Eleven", "Lawson", "FamilyMart", "Hypermart", "Carrefour", "Giant",
 }
