94732bb2b5b59a897360bc77b03898a7585ab907
hafit <hafit.abekrori@students.amikom.ac.id>
2024-09-10 21:53:04 +0700

    add parent RoomDatabase()

--- app/src/main/java/com/haf/artha/data/local/db/AppDatabase.kt
+++ app/src/main/java/com/haf/artha/data/local/db/AppDatabase.kt
@@ -1,6 +1,8 @@
 package com.haf.artha.data.local.db
 
+
 import androidx.room.Database
+import androidx.room.RoomDatabase
 import com.haf.artha.data.local.entity.AccountEntity
 import com.haf.artha.data.local.entity.CategoryEntity
 import com.haf.artha.data.local.entity.TransactionEntity
@@ -14,6 +16,6 @@ import com.haf.artha.data.local.entity.TransactionEntity
     version = 1,
     exportSchema = false
 )
-abstract class AppDatabase {
+abstract class AppDatabase: RoomDatabase() {
     abstract fun appDao(): AppDao
 }
