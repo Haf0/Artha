8d4c50be6fa0dd3ba88444ec584e1eada4b0cc12
hafit <hafit.abekrori@students.amikom.ac.id>
2024-09-10 21:53:57 +0700

    add dependency ktx room to use flow

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -91,6 +91,7 @@ dependencies {
     implementation("androidx.room:room-runtime:$room_version")
     annotationProcessor("androidx.room:room-compiler:$room_version")
     kapt("androidx.room:room-compiler:$room_version")
+    implementation("androidx.room:room-ktx:$room_version")
 
     implementation ("androidx.datastore:datastore-preferences:1.0.0")
 
