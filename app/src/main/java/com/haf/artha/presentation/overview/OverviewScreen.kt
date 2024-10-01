69d2c4d41378a0004ae20c3bfd14657a08e7db71
hafit <hafit.abekrori@students.amikom.ac.id>
2024-10-01 10:13:00 +0700

    feat: add overview screen starter

---
+++ app/src/main/java/com/haf/artha/presentation/overview/OverviewScreen.kt
@@ -0,0 +1,11 @@
+package com.haf.artha.presentation.overview
+
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.navigation.NavHostController
+
+@Composable
+fun OverviewScreen(modifier: Modifier = Modifier, navController: NavHostController) {
+    Text(text = "Overview Screen")
+}
