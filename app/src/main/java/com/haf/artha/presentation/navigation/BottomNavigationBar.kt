bac301c44f559983163b62dddd327de3916ef0b8
hafit <hafit.abekrori@students.amikom.ac.id>
2024-10-01 10:12:30 +0700

    feat: add bottom navigation bar

---
+++ app/src/main/java/com/haf/artha/presentation/navigation/BottomNavigationBar.kt
@@ -0,0 +1,66 @@
+package com.haf.artha.presentation.navigation
+
+
+import androidx.annotation.DrawableRes
+import androidx.compose.material3.Icon
+import androidx.compose.material3.NavigationBar
+import androidx.compose.material3.NavigationBarItem
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.res.vectorResource
+import androidx.navigation.NavController
+import androidx.navigation.NavGraph.Companion.findStartDestination
+import androidx.navigation.compose.currentBackStackEntryAsState
+import com.haf.artha.R
+
+data class BottomNavigationItem(
+    val screen: Screen,
+    @DrawableRes val icon: Int,
+    val label: String
+) {
+    companion object {
+        val items = listOf(
+            BottomNavigationItem(Screen.Home , R.drawable.home, "Home"),
+            BottomNavigationItem(Screen.Overview, R.drawable.overview, "Overview"),
+            BottomNavigationItem(Screen.Account, R.drawable.bank_account, "Account"),
+            BottomNavigationItem(Screen.Setting, R.drawable.settings, "Setting")
+        )
+    }
+}
+
+
+@Composable
+fun BottomNavigationBar( navController: NavController) {
+
+    val navBackStackEntry by navController.currentBackStackEntryAsState()
+    val currentRoute = navBackStackEntry?.destination?.route
+
+    NavigationBar {
+        BottomNavigationItem.items.forEach { item ->
+            NavigationBarItem(
+                icon = {
+                    Icon(
+                        imageVector = ImageVector.vectorResource(id = item.icon),
+                        contentDescription = item.label
+                    )
+                },
+                label = {
+                    Text(item.label)
+                },
+                onClick = {
+                    navController.navigate(item.screen.route) {
+                        popUpTo(navController.graph.findStartDestination().id) {
+                            saveState = true
+                        }
+                        restoreState = true
+                        launchSingleTop = true
+                    }
+                },
+                selected = currentRoute == item.screen.route
+            )
+        }
+    }
+
+}
