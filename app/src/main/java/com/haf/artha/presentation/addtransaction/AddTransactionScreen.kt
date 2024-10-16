73523887cf3dc77a70492ecd544ba12cf427ec3d
hafit <hafit.abekrori@students.amikom.ac.id>
2024-10-17 01:21:53 +0700

    feat: make preparation file

---
+++ app/src/main/java/com/haf/artha/presentation/addtransaction/AddTransactionScreen.kt
@@ -0,0 +1,12 @@
+package com.haf.artha.presentation.addtransaction
+
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.navigation.NavHostController
+
+@Composable
+fun AddTransactionScreen(
+    navController: NavHostController
+) {
+    Text("Add Transaction")
+}
