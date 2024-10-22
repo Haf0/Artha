353b0b15cedd8f61196c287022c117bdbdf40d38
hafit <hafit.abekrori@students.amikom.ac.id>
2024-10-23 00:37:41 +0700

    chore : separate account, transaction, category dao and use typeconverter for account and transaction

--- app/src/main/java/com/haf/artha/data/local/db/AppDatabase.kt
+++ app/src/main/java/com/haf/artha/data/local/db/AppDatabase.kt
@@ -3,9 +3,11 @@ package com.haf.artha.data.local.db
 
 import androidx.room.Database
 import androidx.room.RoomDatabase
+import androidx.room.TypeConverters
 import com.haf.artha.data.local.entity.AccountEntity
 import com.haf.artha.data.local.entity.CategoryEntity
 import com.haf.artha.data.local.entity.TransactionEntity
+import com.haf.artha.utils.TransactionTypeConverter
 
 @Database(
     entities = [
@@ -16,6 +18,9 @@ import com.haf.artha.data.local.entity.TransactionEntity
     version = 1,
     exportSchema = false
 )
+@TypeConverters(TransactionTypeConverter::class)
 abstract class AppDatabase: RoomDatabase() {
-    abstract fun appDao(): AppDao
+    abstract fun categoryDao(): CategoryDao
+    abstract fun accountDao(): AccountDao
+    abstract fun transactionDao(): TransactionDao
 }
