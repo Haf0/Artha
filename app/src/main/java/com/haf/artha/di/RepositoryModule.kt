98a64726fa9fced0a270ecf7440519242aad3675
hafit <hafit.abekrori@students.amikom.ac.id>
2024-10-25 02:25:25 +0700

    feat : module inject for repository

---
+++ app/src/main/java/com/haf/artha/di/RepositoryModule.kt
@@ -0,0 +1,40 @@
+package com.haf.artha.di
+
+import com.haf.artha.data.local.AccountRepository
+import com.haf.artha.data.local.CategoryRepository
+import com.haf.artha.data.local.TransactionRepository
+import com.haf.artha.data.local.db.AccountDao
+import com.haf.artha.data.local.db.CategoryDao
+import com.haf.artha.data.local.db.TransactionDao
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object RepositoryModule {
+
+
+    @Provides
+    @Singleton
+    fun transactionRepository(
+        transactionDao: TransactionDao,
+        accountDao: AccountDao
+    ): TransactionRepository {
+        return TransactionRepository(transactionDao, accountDao)
+    }
+
+    @Provides
+    @Singleton
+    fun provideCategoryRepository(categoryDao: CategoryDao): CategoryRepository {
+        return CategoryRepository(categoryDao)
+    }
+
+    @Provides
+    @Singleton
+    fun provideAccountRepository(accountDao: AccountDao): AccountRepository {
+        return AccountRepository(accountDao)
+    }
+}
