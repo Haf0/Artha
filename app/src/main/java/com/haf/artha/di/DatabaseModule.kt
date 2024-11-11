75b919aa0f60c9910f4bb8d66b56532ba8191438
hafit <hafit.abekrori@students.amikom.ac.id>
2024-11-12 00:36:22 +0700

    fix: add database builder to module

--- app/src/main/java/com/haf/artha/di/DatabaseModule.kt
+++ app/src/main/java/com/haf/artha/di/DatabaseModule.kt
@@ -1,5 +1,7 @@
 package com.haf.artha.di
 
+import android.content.Context
+import androidx.room.Room
 import com.haf.artha.data.local.db.AccountDao
 import com.haf.artha.data.local.db.AppDatabase
 import com.haf.artha.data.local.db.CategoryDao
@@ -7,12 +9,26 @@ import com.haf.artha.data.local.db.TransactionDao
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
 import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
 
 
 @Module
 @InstallIn(SingletonComponent::class)
 object DatabaseModule {
+
+    @Provides
+    @Singleton
+    fun provideAppDatabase(
+        @ApplicationContext context: Context
+    ): AppDatabase {
+        return Room.databaseBuilder(
+            context,
+            AppDatabase::class.java,
+            "artha_database"
+        ).fallbackToDestructiveMigration().build()
+    }
 
     @Provides
     fun provideAccountDao(database: AppDatabase): AccountDao {
