b4550a444ed930276b167dbcaf53bac81031df08
hafit <hafit.abekrori@students.amikom.ac.id>
2024-11-16 03:48:10 +0700

    fix: add transfer category

--- app/src/main/java/com/haf/artha/presentation/onboarding/setCategories/SetCategoryViewModel.kt
+++ app/src/main/java/com/haf/artha/presentation/onboarding/setCategories/SetCategoryViewModel.kt
@@ -7,12 +7,13 @@ import com.haf.artha.data.local.entity.CategoryEntity
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.launch
 import javax.inject.Inject
+import kotlin.random.Random
 
 @HiltViewModel
 class SetCategoryViewModel @Inject constructor(
     private val categoryRepository: CategoryRepository
 ): ViewModel() {
     fun insertCategory(categories: List<CategoryEntity>) {
-        viewModelScope.launch { categoryRepository.insertCategory(categories.distinct()) }
+        viewModelScope.launch { categoryRepository.insertCategory(categories.plus(CategoryEntity(0,"Transfer", Random.nextInt(0xFFFFFF) or (0xFF shl 24) )).distinct()) }
     }
 }
