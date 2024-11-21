477c1b31fc2c715194b5e0eb57b477b03b653d91
hafit <hafit.abekrori@students.amikom.ac.id>
2024-11-22 03:33:03 +0700

    feat: add function to get total income, expense, and total balance

--- app/src/main/java/com/haf/artha/data/local/db/TransactionDao.kt
+++ app/src/main/java/com/haf/artha/data/local/db/TransactionDao.kt
@@ -7,6 +7,7 @@ import androidx.room.OnConflictStrategy
 import androidx.room.Query
 import androidx.room.Update
 import com.haf.artha.data.local.entity.TransactionEntity
+import com.haf.artha.data.local.model.TransactionType
 import kotlinx.coroutines.flow.Flow
 
 
@@ -30,6 +31,22 @@ interface TransactionDao {
     @Query("SELECT * FROM transactions WHERE date BETWEEN :startOfDay AND :endOfDay")
     fun getTransactionsBySpecificDay(startOfDay: Long, endOfDay: Long): Flow<List<TransactionEntity>>
 
+    @Query("SELECT * FROM transactions WHERE date BETWEEN :startOfWeek AND :endOfWeek")
+    fun getTransactionsBySpecificWeek(startOfWeek: Long, endOfWeek: Long): Flow<List<TransactionEntity>>
+
     @Query("SELECT * FROM transactions WHERE date BETWEEN :startOfMonth AND :endOfMonth")
     fun getTransactionsBySpecificMonth(startOfMonth: Long, endOfMonth: Long): Flow<List<TransactionEntity>>
+
+    //SUM this month Income transactions
+    @Query("SELECT SUM(amount) FROM transactions WHERE type = :type AND date BETWEEN :startOfMonth AND :endOfMonth")
+    fun getTotalIncomeThisMonth(startOfMonth: Long, endOfMonth: Long, type:TransactionType): Flow<Double>
+
+    //SUM this month Expense transactions
+    @Query("SELECT SUM(amount) FROM transactions WHERE type = 'Pengeluaran' AND date BETWEEN :startOfMonth AND :endOfMonth")
+    fun getTotalExpenseThisMonth(startOfMonth: Long, endOfMonth: Long): Flow<Double>
+
+    @Query("SELECT * FROM transactions ORDER BY date DESC LIMIT 15")
+    fun getRecentTransactions(): Flow<List<TransactionEntity>>
+
+
 }
