d9b413333e5b861515872415a199e75e2761c086
hafit <hafit.abekrori@students.amikom.ac.id>
2024-11-22 03:29:21 +0700

    feat: add date utils to get today date,this month, and this week as Long variable

--- app/src/main/java/com/haf/artha/utils/DateUtils.kt
+++ app/src/main/java/com/haf/artha/utils/DateUtils.kt
@@ -1,8 +1,5 @@
 
-import android.annotation.SuppressLint
 import java.text.SimpleDateFormat
-import java.time.LocalDate
-import java.time.ZoneId
 import java.util.Calendar
 import java.util.Date
 import java.util.Locale
@@ -48,16 +45,57 @@ object DateUtils {
 
         return Pair(startOfMonth, endOfMonth)
     }
+
+    fun getStartAndEndOfCurrentWeek(timestamp: Long): Pair<Long, Long> {
+        val calendar = Calendar.getInstance().apply {
+            timeInMillis = timestamp
+            set(Calendar.DAY_OF_WEEK, firstDayOfWeek)
+            set(Calendar.HOUR_OF_DAY, 0)
+            set(Calendar.MINUTE, 0)
+            set(Calendar.SECOND, 0)
+            set(Calendar.MILLISECOND, 0)
+        }
+        val startOfWeek = calendar.timeInMillis
+        calendar.add(Calendar.WEEK_OF_YEAR, 1)
+        val endOfWeek = calendar.timeInMillis - 1
+        return Pair(startOfWeek, endOfWeek)
+    }
+
+    fun getStartAndEndOfLastWeek(timestamp: Long): Pair<Long, Long> {
+        val calendar = Calendar.getInstance().apply {
+            timeInMillis = timestamp
+            set(Calendar.DAY_OF_WEEK, firstDayOfWeek)
+            add(Calendar.WEEK_OF_YEAR, -1)
+            set(Calendar.HOUR_OF_DAY, 0)
+            set(Calendar.MINUTE, 0)
+            set(Calendar.SECOND, 0)
+            set(Calendar.MILLISECOND, 0)
+        }
+        val startOfLastWeek = calendar.timeInMillis
+        calendar.add(Calendar.WEEK_OF_YEAR, 1)
+        val endOfLastWeek = calendar.timeInMillis - 1
+        return Pair(startOfLastWeek, endOfLastWeek)
+    }
+
     fun Long.toFormattedDate(): String {
         val date = Date(this)
-        val format = SimpleDateFormat("EEEE, dd MM yyyy", Locale.getDefault())
+        val format = SimpleDateFormat("EEEE, dd-MM-yyyy", Locale("id", "ID"))
         return format.format(date)
     }
 
-    @SuppressLint("NewApi")
-    fun LocalDate.toTimestamp(): Long {
-        return this.atStartOfDay(ZoneId.systemDefault())
-            .toInstant()
-            .toEpochMilli()
+    fun getTodayDate(): String {
+        val formatter = SimpleDateFormat("ddMMyyyy", Locale.getDefault())
+        return formatter.format(Date())
     }
+
+    fun getTodayTimestamp(): Long {
+        return convertDateStringToLong(getTodayDate())
+    }
+
+    fun convertDateStringToLong(dateString: String): Long {
+        val formatter = SimpleDateFormat("ddMMyyyy", Locale.getDefault())
+        val date = formatter.parse(dateString)
+        return date?.time ?: 0L
+    }
+
 }
