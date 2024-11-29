45313f3bce61519c887981341c223d643db83bff
hafit <hafit.abekrori@students.amikom.ac.id>
2024-11-29 11:09:35 +0700

    feat: add color option

--- app/src/main/java/com/haf/artha/ui/theme/Color.kt
+++ app/src/main/java/com/haf/artha/ui/theme/Color.kt
@@ -9,3 +9,88 @@ val Pink80 = Color(0xFFEFB8C8)
 val Purple40 = Color(0xFF6650a4)
 val PurpleGrey40 = Color(0xFF625b71)
 val Pink40 = Color(0xFF7D5260)
+
+
+val slateBlue = Color(0xFF6A7B9D)
+val lavenderMist = Color(0xFFEADDFF)
+val dustyBlue = Color(0xFF625B71)
+val paleLilac = Color(0xFFE8DEF8)
+val steelBlue = Color(0xFF4682B4)
+val lightPeriwinkle = Color(0xFFD1E0F1)
+val mutedTeal = Color(0xFF4B8B8E)
+val paleSkyBlue = Color(0xFFA3C1DA)
+val mistyRose = Color(0xFFFFE4E1)
+val coolLavender = Color(0xFFA7C6ED)
+val powderBlue = Color(0xFFB0E0E6)
+val softSage = Color(0xFFB7C9A8)
+val skyBlue = Color(0xFF87CEEB)
+val lightBlush = Color(0xFFF1C0C7)
+val mutedAqua = Color(0xFFA0D3E8)
+val seaFoamGreen = Color(0xFF9FE2BF)
+val paleGreen = Color(0xFF98FB98)
+val lightSteelBlue = Color(0xFFB0C4DE)
+val softMint = Color(0xFFA8E1D4)
+val fadedDenim = Color(0xFF6A9BD1)
+val palePeach = Color(0xFFFFDAB9)
+val softCoral = Color(0xFFF08080)
+val coral = Color(0xFFFF7F50)
+val turquoise = Color(0xFF40E0D0)
+val lightSeaGreen = Color(0xFF20B2AA)
+val lightCoral = Color(0xFFF08080)
+val goldenrod = Color(0xFFDAA520)
+val lightGoldenrodYellow = Color(0xFFFAFAD2)
+val mediumOrchid = Color(0xFFBA55D3)
+val lightSlateBlue = Color(0xFF8470FF)
+val chartreuse = Color(0xFF7FFF00)
+val mediumSeaGreen = Color(0xFF3CB371)
+val salmon = Color(0xFFFA8072)
+val darkSalmon = Color(0xFFE9967A)
+val mistyMint = Color(0xFFB2E0D5)
+val lightOrchid = Color(0xFFE6A8D7)
+val springGreen = Color(0xFF00FF7F)
+val mediumVioletRed = Color(0xFFC71585)
+val tomato = Color(0xFFFF6347)
+val lightSalmonPink =Color(0xFFFF9999)
+
+val listColorOption = listOf(
+    slateBlue,
+    lavenderMist,
+    dustyBlue,
+    paleLilac,
+    steelBlue,
+    lightPeriwinkle,
+    mutedTeal,
+    paleSkyBlue,
+    mistyRose,
+    coolLavender,
+    powderBlue,
+    softSage,
+    skyBlue,
+    lightBlush,
+    mutedAqua,
+    seaFoamGreen,
+    paleGreen,
+    lightSteelBlue,
+    softMint,
+    fadedDenim,
+    palePeach,
+    softCoral,
+    coral,
+    turquoise,
+    lightSeaGreen,
+    lightCoral,
+    goldenrod,
+    lightGoldenrodYellow,
+    mediumOrchid,
+    lightSlateBlue,
+    chartreuse,
+    mediumSeaGreen,
+    salmon,
+    darkSalmon,
+    mistyMint,
+    lightOrchid,
+    springGreen,
+    mediumVioletRed,
+    tomato,
+    lightSalmonPink
+)
