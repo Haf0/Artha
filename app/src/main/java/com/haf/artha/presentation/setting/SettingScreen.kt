8c21b7c2c11436b8e81e28815e2597991b4a9702
hafit <hafit.abekrori@students.amikom.ac.id>
2024-11-29 11:08:29 +0700

    feat: setting screen

--- app/src/main/java/com/haf/artha/presentation/setting/SettingScreen.kt
+++ app/src/main/java/com/haf/artha/presentation/setting/SettingScreen.kt
@@ -1,14 +1,70 @@
 package com.haf.artha.presentation.setting
 
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
 import androidx.navigation.NavHostController
+import com.haf.artha.navigation.Screen
 
 @Composable
 fun SettingScreen(
     modifier: Modifier = Modifier,
     navController: NavHostController
 ) {
-    Text("Setting")
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(16.dp)
+    ) {
+        SettingItem(
+            title = "Profile"
+        )
+        SettingItem(
+            title = "Category",
+            onClick = {
+                navController.navigate(Screen.Category.route)
+            }
+        )
+        SettingItem(
+            title = "Account"
+        )
+        SettingItem(
+            title = "Transaction"
+        )
+        SettingItem(
+            title = "Overview"
+        )
+    }
 }
+
+
+@Composable
+fun SettingItem(
+    modifier: Modifier = Modifier,
+    title: String,
+    onClick: () -> Unit = {}
+) {
+    //item that can be clicked to navigate to a new screen
+    Column(
+        Modifier.fillMaxWidth().padding(vertical = 8.dp)
+            .clickable {
+                onClick()
+            }
+    ) {
+        Text(title)
+        HorizontalDivider(
+            modifier = Modifier.padding(top = 8.dp),
+            thickness = 1.dp,
+            color = Color.Gray
+        )
+    }
+}
+
