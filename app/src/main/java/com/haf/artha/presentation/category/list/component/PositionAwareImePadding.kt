41d90ed04e119f80eaf518b610d00ddab1f58a80
hafit <hafit.abekrori@students.amikom.ac.id>
2024-12-10 00:52:00 +0700

    feature: add position aware ime padding modifier

---
+++ app/src/main/java/com/haf/artha/presentation/category/list/component/PositionAwareImePadding.kt
@@ -0,0 +1,28 @@
+package com.haf.artha.presentation.category.list.component
+
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.consumeWindowInsets
+import androidx.compose.foundation.layout.imePadding
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.composed
+import androidx.compose.ui.layout.findRootCoordinates
+import androidx.compose.ui.layout.onGloballyPositioned
+import androidx.compose.ui.layout.positionInWindow
+import androidx.compose.ui.platform.LocalDensity
+import androidx.compose.ui.unit.dp
+
+fun Modifier.positionAwareImePadding() = composed {
+    var consumePadding by remember { mutableStateOf(0) }
+    onGloballyPositioned { coordinates ->
+        val rootCoordinate = coordinates.findRootCoordinates()
+        val bottom = coordinates.positionInWindow().y + coordinates.size.height
+
+        consumePadding = (rootCoordinate.size.height - bottom).toInt()
+    }
+        .consumeWindowInsets(PaddingValues(bottom = ((consumePadding / LocalDensity.current.density)-10).dp))
+        .imePadding()
+}
