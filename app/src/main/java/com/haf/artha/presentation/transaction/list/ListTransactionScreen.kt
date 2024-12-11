ef10ae9aeaee6299579f952c092519d439817a96
hafit <hafit.abekrori@students.amikom.ac.id>
2024-12-12 02:37:28 +0700

    feat: implementing list transaction

--- app/src/main/java/com/haf/artha/presentation/transaction/list/ListTransactionScreen.kt
+++ app/src/main/java/com/haf/artha/presentation/transaction/list/ListTransactionScreen.kt
@@ -1,9 +1,27 @@
+@file:OptIn(ExperimentalFoundationApi::class)
+
 package com.haf.artha.presentation.transaction.list
 
+import DateUtils.toFormattedDate
+import androidx.compose.foundation.ExperimentalFoundationApi
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
+import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavHostController
+import com.haf.artha.presentation.home.component.TransactionHistoryItem
+import com.haf.artha.utils.CurrencyUtils
 
 @Composable
 fun ListTransactionScreen(
@@ -15,7 +33,34 @@ fun ListTransactionScreen(
 
 @Composable
 fun ListTransactionContent(
-    modifier: Modifier = Modifier
+    modifier: Modifier = Modifier,
+    viewModel: ListTransactionViewModel = hiltViewModel()
 ) {
-    Text("List Transaction")
+    val transactions by viewModel.transactions.collectAsState(emptyList())
+
+    LazyColumn {
+        val groupedTransactions = transactions.groupBy { it.date.toFormattedDate() }
+        groupedTransactions.forEach { (date, transactions) ->
+            stickyHeader {
+                Text(
+                    text = date,
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(top = 8.dp)
+                        .clip(RoundedCornerShape(8.dp))
+                        .background(Color(0xD7FFD8E4))
+                        .padding(8.dp)
+                )
+            }
+            items(transactions) { transaction ->
+                TransactionHistoryItem(
+                    intColor = transaction.categoryId,
+                    title = transaction.name,
+                    date = transaction.date.toFormattedDate(),
+                    amount = CurrencyUtils.formatAmount(transaction.amount),
+                    transactionType = transaction.type
+                )
+            }
+        }
+    }
 }
