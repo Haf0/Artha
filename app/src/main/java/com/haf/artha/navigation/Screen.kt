c7da54a2b35e3084c29feae8d140f78c08984e57
hafit <hafit.abekrori@students.amikom.ac.id>
2024-12-12 01:57:14 +0700

    feat: adding detail transaction route

--- app/src/main/java/com/haf/artha/navigation/Screen.kt
+++ app/src/main/java/com/haf/artha/navigation/Screen.kt
@@ -12,16 +12,14 @@ sealed class Screen(
     object AddAccount : Screen("addAccount/accountId={accountId}"){
         fun createRoute(accountId: Int? = null) = "addAccount/accountId=$accountId"
     }
-
     object Transaction : Screen("transaction")
+    object DetailTransaction : Screen("transaction/{transactionId}"){
+        fun createRoute(transactionId: Int) = "transaction/$transactionId"
+    }
     object AddTransaction : Screen("addTransaction")
-
     object Category : Screen("category")
     object Setting : Screen("setting")
     object About : Screen("about")
-    object Notification : Screen("notification")
-    object EditAccount : Screen("editAccount")
-    object EditCategory : Screen("editCategory")
     object EditTransaction : Screen("editTransaction")
     object Overview : Screen("overview")
     object SplashScreen : Screen("splashScreen")
