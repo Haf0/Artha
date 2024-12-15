d58aeefe97ac673f38bdad011d167717adc22ae0
hafit <hafit.abekrori@students.amikom.ac.id>
2024-12-15 22:18:40 +0700

    feat: add delete transaction by id

--- app/src/main/java/com/haf/artha/presentation/transaction/detail/DetailTransactionViewModel.kt
+++ app/src/main/java/com/haf/artha/presentation/transaction/detail/DetailTransactionViewModel.kt
@@ -1,8 +1,11 @@
 package com.haf.artha.presentation.transaction.detail
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.haf.artha.data.local.TransactionRepository
+import com.haf.artha.data.model.TransactionDetail
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 @HiltViewModel
 class DetailTransactionViewModel @Inject constructor(
@@ -10,4 +13,11 @@ class DetailTransactionViewModel @Inject constructor(
 ): ViewModel() {
 
     suspend fun getTransaction(id: Int) = transactionRepository.getTransactionById(id)
+    fun deleteTransaction(transactionDetail: TransactionDetail) {
+        viewModelScope.launch {
+            transactionRepository.deleteTransaction(transactionDetail)
+
+        }
+
+    }
 }
