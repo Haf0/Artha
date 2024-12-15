c7c115e1b869d2166fd8cf67fccf916e637d674b
hafit <hafit.abekrori@students.amikom.ac.id>
2024-12-15 22:11:53 +0700

    fix: force to lightmode because yeah darkmode not implemented yet

--- app/src/main/java/com/haf/artha/ui/theme/Theme.kt
+++ app/src/main/java/com/haf/artha/ui/theme/Theme.kt
@@ -2,7 +2,6 @@ package com.haf.artha.ui.theme
 
 import android.app.Activity
 import android.os.Build
-import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.darkColorScheme
 import androidx.compose.material3.dynamicLightColorScheme
@@ -38,7 +37,7 @@ private val LightColorScheme = lightColorScheme(
 
 @Composable
 fun ArthaTheme(
-    darkTheme: Boolean = isSystemInDarkTheme(),
+    darkTheme: Boolean = false,
     // Dynamic color is available on Android 12+
     dynamicColor: Boolean = true,
     content: @Composable () -> Unit
