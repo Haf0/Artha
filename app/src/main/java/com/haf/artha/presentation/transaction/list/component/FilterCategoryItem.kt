e2e775b17450f4f0ee16fa9f476054d22723f850
hafit <hafit.abekrori@students.amikom.ac.id>
2024-12-21 08:10:41 +0700

    fix: improve the category item filter

--- app/src/main/java/com/haf/artha/presentation/transaction/list/component/FilterCategoryItem.kt
+++ app/src/main/java/com/haf/artha/presentation/transaction/list/component/FilterCategoryItem.kt
@@ -14,37 +14,33 @@ import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
-import com.haf.artha.data.local.entity.CategoryEntity
 
 @Composable
-fun FilterCategpryItem(
+fun FilterCategoryItem(
     modifier: Modifier = Modifier,
-    list: MutableList<CategoryEntity>,
-    index: Int,
+    color: Int,
+    name: String,
+    isSelected : Boolean,
     onClick: () -> Unit
 
 ) {
-    val selected = remember { mutableStateOf(false) }
+
     Card(
         shape = RoundedCornerShape(8.dp),
         colors = CardDefaults.cardColors(
-            containerColor = if (selected.value) {
+            containerColor = if (isSelected) {
                 MaterialTheme.colorScheme.tertiaryContainer
             } else {
                 MaterialTheme.colorScheme.primaryContainer
             }
         ),
         modifier = modifier
-            .padding(4.dp)
             .clickable {
-                selected.value = !selected.value
                 onClick()
             },
         elevation = CardDefaults.cardElevation(4.dp)
@@ -61,10 +57,10 @@ fun FilterCategpryItem(
                     .padding(end = 8.dp)
                     .clip(CircleShape)
                     .size(20.dp)
-                    .background(Color(list[index].color))
+                    .background(Color(color))
             )
             Text(
-                text = list[index].name,
+                text = name,
                 style = MaterialTheme.typography.bodyMedium,
             )
         }
