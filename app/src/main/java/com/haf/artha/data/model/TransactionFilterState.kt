c0b90d68fe1b89caf9315742eb4b0f34e09d0f0d
hafit <hafit.abekrori@students.amikom.ac.id>
2024-12-21 08:08:25 +0700

    feat: change list transaction type variable

--- app/src/main/java/com/haf/artha/data/model/TransactionFilterState.kt
+++ app/src/main/java/com/haf/artha/data/model/TransactionFilterState.kt
@@ -1,12 +1,10 @@
 package com.haf.artha.data.model
 
-import com.haf.artha.data.local.model.TransactionType
-
 data class TransactionFilterState(
     val searchText: String? = null,
     val startDate: Long?= null,
     val endDate: Long? = null,
-    val transactionTypes: List<TransactionType>? = null,
+    val transactionTypes: List<String>? = null,
     val categories: List<Int>? = null,
     val minAmount: Double? = null,
     val maxAmount: Double? = null,
