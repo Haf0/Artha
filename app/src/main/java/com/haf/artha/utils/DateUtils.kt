8af4ad24bf601a022fec8c064329fd8dc4d12a06
hafit <hafit.abekrori@students.amikom.ac.id>
2024-12-23 10:41:36 +0700

    feat: adding function to get start and end of day for query

--- app/src/main/java/com/haf/artha/utils/DateUtils.kt
+++ app/src/main/java/com/haf/artha/utils/DateUtils.kt
@@ -104,4 +104,26 @@ object DateUtils {
         return date?.time ?: 0L
     }
 
+    fun getEndOfDay(endDate: Long): Long {
+        val calendar = Calendar.getInstance().apply {
+            timeInMillis = endDate
+            set(Calendar.HOUR_OF_DAY, 23)
+            set(Calendar.MINUTE, 59)
+            set(Calendar.SECOND, 59)
+            set(Calendar.MILLISECOND, 999)
+        }
+        return calendar.timeInMillis
+    }
+
+    fun getStartOfDay(startDate: Long): Long {
+        val calendar = Calendar.getInstance().apply {
+            timeInMillis = startDate
+            set(Calendar.HOUR_OF_DAY, 0)
+            set(Calendar.MINUTE, 0)
+            set(Calendar.SECOND, 0)
+            set(Calendar.MILLISECOND, 0)
+        }
+        return calendar.timeInMillis
+    }
+
 }
