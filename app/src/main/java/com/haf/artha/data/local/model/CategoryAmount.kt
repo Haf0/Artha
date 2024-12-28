71510f3eb589cfa8962e9323ec805cab9b5b23e2
hafit <hafit.abekrori@students.amikom.ac.id>
2024-12-28 14:19:29 +0700

    feat: add model for getCategory grouped by amount,year, and month

---
+++ app/src/main/java/com/haf/artha/data/local/model/CategoryAmount.kt
@@ -0,0 +1,11 @@
+package com.haf.artha.data.local.model
+
+data class CategoryAmount(
+    val categoryId: Int,
+    val categoryName: String,
+    val categoryColor: Int,
+    val year: String,
+    val month: String,
+    val totalAmount: Double
+)
+
