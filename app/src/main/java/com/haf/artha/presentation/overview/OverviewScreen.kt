ee8c807ac291ee09067cf67ccd749ab2472ead4c
hafit <hafit.abekrori@students.amikom.ac.id>
2024-12-30 10:31:53 +0700

    feat: add function logic to change year and month

--- app/src/main/java/com/haf/artha/presentation/overview/OverviewScreen.kt
+++ app/src/main/java/com/haf/artha/presentation/overview/OverviewScreen.kt
@@ -1,18 +1,62 @@
 package com.haf.artha.presentation.overview
 
+import DateUtils
+import androidx.compose.foundation.layout.Column
+import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableIntStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.navigation.NavHostController
 
 @Composable
 fun OverviewScreen(modifier: Modifier = Modifier, navController: NavHostController) {
-    Text(text = "Overview Screen")
+    OverViewContent()
 }
 
 
 @Composable
 fun OverViewContent(modifier: Modifier = Modifier) {
+    val time = DateUtils.getTodayMonthAndYear()
+    var years by remember { mutableIntStateOf(time.first) }
+    var months by remember { mutableIntStateOf(time.second) }
+
+    Column {
+        Text(text = "$months $years")
+        Button(
+            onClick = {
+                when (months) {
+                    12 -> {
+                        years += 1
+                        months = 1
+                    }
+                    else -> {
+                        months += 1
+                    }
+                }
+            }
+        ) {
+            Text(text = "Next")
+        }
+        Button(
+            onClick = {
+                when (months) {
+                    1 -> {
+                        years -= 1
+                        months = 12
+                    }
+                    else -> {
+                        months -= 1
+                    }
+                }
+            }
+        ) {
+            Text(text = "Previous")
+        }
+    }
 
 }
 
