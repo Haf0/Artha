33e10bd944c48a6c7896454a16bec08129f39ced
hafit <hafit.abekrori@students.amikom.ac.id>
2025-10-23 22:45:57 +0700

    feat: add profit bitmap for flexing

--- app/src/main/java/com/haf/artha/presentation/overview/OverviewScreen.kt
+++ app/src/main/java/com/haf/artha/presentation/overview/OverviewScreen.kt
@@ -1,8 +1,14 @@
 package com.haf.artha.presentation.overview
 
 import DateUtils
+import android.annotation.SuppressLint
+import android.os.Build
+import androidx.annotation.RequiresApi
 import androidx.compose.foundation.layout.Column
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Share
 import androidx.compose.material3.Button
+import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
@@ -10,19 +16,28 @@ import androidx.compose.runtime.mutableIntStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
 import androidx.navigation.NavHostController
+import com.haf.artha.presentation.overview.component.createIncomeExpenseBitmap
+import com.haf.artha.presentation.overview.component.shareBitmap
+import java.time.Month
 
+@SuppressLint("NewApi")
 @Composable
 fun OverviewScreen(modifier: Modifier = Modifier, navController: NavHostController) {
     OverViewContent()
 }
 
 
+@RequiresApi(Build.VERSION_CODES.O)
 @Composable
 fun OverViewContent(modifier: Modifier = Modifier) {
     val time = DateUtils.getTodayMonthAndYear()
     var years by remember { mutableIntStateOf(time.first) }
     var months by remember { mutableIntStateOf(time.second) }
+
+    val context = LocalContext.current
+    val bitmap = createIncomeExpenseBitmap(context, "+20%", "-10%")
 
     Column {
         Text(text = "$months $years")
@@ -55,6 +70,18 @@ fun OverViewContent(modifier: Modifier = Modifier) {
             }
         ) {
             Text(text = "Previous")
+        }
+
+        val monthName = Month.of(months).name
+        Button(
+            onClick = {
+                shareBitmap(context = context, bitmap = bitmap, title= "$monthName $years")
+            }
+        ) {
+            Icon(
+                Icons.Default.Share,
+                contentDescription = "Share"
+            )
         }
     }
 
---
+++ app/src/main/java/com/haf/artha/presentation/overview/component/ShareOverviewBitmap.kt
@@ -0,0 +1,22 @@
+package com.haf.artha.presentation.overview.component
+
+import android.content.Context
+import android.content.Intent
+import android.graphics.Bitmap
+import android.net.Uri
+import androidx.core.content.FileProvider
+import java.io.File
+import java.io.FileOutputStream
+
+
+fun shareBitmap(context: Context, bitmap: Bitmap, title: String) {
+    val file = File(context.cacheDir, "$title-report.png")
+    FileOutputStream(file).use { out -> bitmap.compress(Bitmap.CompressFormat.PNG, 100, out) }
+    val uri: Uri = FileProvider.getUriForFile(context, "${context.packageName}.provider", file)
+    val intent = Intent(Intent.ACTION_SEND).apply {
+        type = "image/png"
+        putExtra(Intent.EXTRA_STREAM, uri)
+        addFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION)
+    }
+    context.startActivity(Intent.createChooser(intent, "Share Comparison"))
+}
